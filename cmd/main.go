540c97ebc1e807ff073a988e6c09895424d340a2
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-04-28 20:36:49 +0000

    backend auth logic

---
+++ cmd/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/kallepan/go-jwt/common/controllers"
+	"gitlab.com/kallepan/go-jwt/common/database"
+	"gitlab.com/kallepan/go-jwt/common/middlewares"
+	"gitlab.com/kallepan/go-jwt/jwt"
+	"gitlab.com/kallepan/go-jwt/utils"
+)
+
+func main() {
+	connectionString := utils.GetConnectionString()
+	database.Connect(connectionString)
+
+	router := initRouter()
+	router.Run(":8080")
+}
+
+func initRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.NoRoute(middlewares.NoRouteHandler)
+	router.Use(middlewares.ErrorHandler)
+	router.Use(middlewares.CORSMiddleware())
+	router.SetTrustedProxies(strings.Split(utils.GetValueFromEnv("TRUSTED_PROXIES", ","), ","))
+
+	auth := router.Group("/api")
+	{
+		auth.POST("/token", jwt.GenerateJWTTokenController)
+		auth.POST("/register", jwt.RegisterUser)
+	}
+
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/ping", controllers.Ping)
+	}
+
+	secured := router.Group("/api/v1")
+	secured.Use(middlewares.AuthMiddleware())
+	{
+		// Add your routes here
+	}
+
+	return router
+}
---
+++ common/middlewares/auth.go
@@ -0,0 +1,34 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gitlab.com/kallepan/go-jwt/jwt"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString := ctx.GetHeader("Authorization")
+		if tokenString == "" {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+			return
+		}
+
+		err := jwt.ValidateJWTToken(tokenString)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+
+		// Set user_id if possible
+		user_id, err := jwt.GetUserIdFromToken(ctx.GetHeader("Authorization"))
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			return
+		}
+		ctx.Set("user_id", user_id)
+
+		ctx.Next()
+	}
+}
